8eff8dbe03b56c9033c213a8b92e6d831da1e945
HughesTT <100058635+HughesTT@users.noreply.github.com>
2024-11-20 11:05:48 +0800

    git add

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    This is a Card
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '文字區域'// Define any necessary data here
+    }
+  }
+}
+</script>
